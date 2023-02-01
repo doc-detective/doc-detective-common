fe1a88736930d1fdd2c62610993fddd7cd795708
hawkeyexl <hawkeyexl@gmail.com>
2023-02-01 15:43:51 -0800

    Restructured to have a test per schema example

--- test/schema.test.js
+++ test/schema.test.js
@@ -3,12 +3,14 @@ const { validate } = require("../src/validate");
 
 // Loop through JSON schemas
 for (const [key, value] of Object.entries(schema)) {
-  test(`${key} schema examples pass validation`, () => {
+  test(`${key} schema has one or more examples`, () => {
     // Schema needs one or more examples
     expect(value.examples).toBeTruthy();
     expect(value.examples.length).toBeGreaterThanOrEqual(1);
-    // Loop through and validate schema examples
-    value.examples.forEach((example) => {
+  });
+  // Loop through and validate schema examples
+  value.examples.forEach((example, index) => {
+    test(`${key} example with index ${index} passes validation`, () => {
       expect(validate(key, example)).toBe(true);
     });
   });
