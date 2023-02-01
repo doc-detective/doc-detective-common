64339e58e3d085a7a091cfbd90c8e367d91ca0f6
hawkeyexl <hawkeyexl@gmail.com>
2023-02-01 09:56:59 -0800

    Test that all schema examples validate against schema definitions

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,8 +1,15 @@
-const { schema } = require("..");
-const Ajv = require("ajv");
-const ajv = new Ajv();
-const validate = ajv.compile(schema.v1.actions.runShell);
+const { schema } = require("../src/schema");
+const { validate } = require("../src/validate");
 
-test("schema examples pass validation", () => {
-  expect(validate(schema.v1.actions.runShell.examples[0])).toBe(true);
-});
+// Loop through JSON schemas
+for (const [key, value] of Object.entries(schema)) {
+  test(`${key} schema examples pass validation`, () => {
+    // Schema needs one or more examples
+    expect(value.examples).toBeTruthy();
+    expect(value.examples.length).toBeGreaterThanOrEqual(1);
+    // Loop through and validate schema examples
+    value.examples.forEach((example) => {
+      expect(validate(key, example)).toBe(true);
+    });
+  });
+}
