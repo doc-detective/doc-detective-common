c2cd6672546b7030ebb924a87fdf6620429379bc
hawkeyexl <hawkeyexl@gmail.com>
2023-02-01 09:58:23 -0800

    Updated exports

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-const v1_analytics = require("./schema/v1/analytics.schema.json");
-const v1_runShell = require("./schema/v1/actions/runShell.schema.json");
-
-const schema = {
-  checkLink_v1: require("./schema/v1/actions/checkLink.schema.json"),
-  stopRecording_v1: require("./schema/v1/actions/stopRecording.schema.json"),
-  runShell_v1: require("./schema/v1/actions/runShell.schema.json"),
-  analytics_v1: require("./schema/v1/analytics.schema.json"),
-};
+const { schema } = require("./src/schema");
+const { validate } = require("./src/validate");
 
 exports.schema = schema;
+exports.validate = validate;
