a19b2e9c5188e8b6942207078cd968fe9437f1b3
hawkeyexl <hawkeyexl@gmail.com>
2023-03-05 15:05:11 -0800

    Added extra schema formats, keywords, and errors

--- src/validate.js
+++ src/validate.js
@@ -1,6 +1,24 @@
 const { schemas } = require("./schema");
 const Ajv = require("ajv");
-const ajv = new Ajv({useDefaults: true});
+// Ajv extra formats: https://ajv.js.org/packages/ajv-formats.html
+const addFormats = require("ajv-formats");
+// Ajv extra keywords: https://ajv.js.org/packages/ajv-keywords.html
+const addKeywords = require("ajv-keywords");
+// Ajv custom errors: https://ajv.js.org/packages/ajv-errors.html
+const addErrors = require("ajv-errors");
+const uuid = require("uuid");
+
+// Configure base Ajv
+const ajv = new Ajv({ useDefaults: true, allErrors: true });
+
+// Enable `uuid` dynamic defult
+const def = require("ajv-keywords/dist/definitions/dynamicDefaults");
+def.DEFAULTS.uuid = () => uuid.v4;
+
+// Enhance Ajv
+addFormats(ajv);
+addKeywords(ajv);
+addErrors(ajv);
 
 // Exports
 exports.validate = validate;
@@ -16,6 +34,6 @@ function validate(schemaKey, object) {
   check = ajv.getSchema(schemaKey);
   result.valid = check(object);
   result.errors = check.errors || "";
-  
+
   return result;
 }
