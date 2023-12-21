f842cda04ccc5dbda2434e5b104fcb38c8caa47c
hawkeyexl <hawkeyexl@gmail.com>
2023-12-21 10:40:03 -0800

    Added moveTo spec

--- src/schemas/dereferenceSchemas.js
+++ src/schemas/dereferenceSchemas.js
@@ -24,6 +24,7 @@ async function dereferenceSchemas() {
     "find_v2.schema.json",
     "goTo_v2.schema.json",
     "httpRequest_v2.schema.json",
+    "moveTo_v2.schema.json",
     "runShell_v2.schema.json",
     "saveScreenshot_v2.schema.json",
     "setVariables_v2.schema.json",
