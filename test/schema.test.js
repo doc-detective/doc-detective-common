e17745d6011b83cd0698ed7e25a7a4dab16c3028
hawkeyexl <hawkeyexl@gmail.com>
2024-10-10 11:37:33 -0700

    Migrate from Jest to Mocha tests

--- test/schema.test.js
+++ test/schema.test.js
@@ -1,21 +1,25 @@
 const { validate, schemas } = require("../src/index");
+const assert = require('assert');
 
 // Loop through JSON schemas
 for (const [key, value] of Object.entries(schemas)) {
-  test(`${key} schema has one or more examples`, () => {
-    // Schema needs one or more examples
-    expect(value.examples).toBeTruthy();
-    expect(value.examples.length).toBeGreaterThanOrEqual(1);
-  });
-  // Loop through and validate schema examples
-  value.examples.forEach((example, index) => {
-    test(`${key} example with index ${index} passes validation`, () => {
-      const validityCheck = validate(key, example);
-      if (validityCheck.errors != "") {
-        console.log(key);
-        console.log(validityCheck.errors);
-      }
-      expect(validityCheck.valid).toBe(true);
+  describe(`${key} schema`, function() {
+    it('has one or more examples', function() {
+      // Schema needs one or more examples
+      assert(value.examples);
+      assert(value.examples.length >= 1);
+    });
+
+    // Loop through and validate schema examples
+    value.examples.forEach((example, index) => {
+      it(`example with index ${index} passes validation`, function() {
+        const validityCheck = validate(key, example);
+        if (validityCheck.errors != "") {
+          console.log(key);
+          console.log(validityCheck.errors);
+        }
+        assert.strictEqual(validityCheck.valid, true);
+      });
     });
   });
 }
